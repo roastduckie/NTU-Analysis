b7099eaade2755250ff1a12116003fbe8738554e
roastduckie <kherring33&@gmail.com>
2020-08-12 13:58:29 -0500

    Fixed output string formatting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 
 shell_steam_pressure = float(input('What is the shell-side steam pressure? '))
@@ -31,53 +30,53 @@ else:
     nuss_coeff = 0.3
 
 reyn = (water_density * water_vel * tube_d_i) / water_visc
-print('The Reynolds number is %f.', reyn)
+print(f'The Reynolds number is {reyn}.')
 
 if reyn > 2500:
     print('The flow is turbulent.')
     nusselt = 0.023 * water_prandtl ** nuss_coeff * reyn ** 0.8
-    print('The Nusselt number is %f', nusselt)
+    print(f'The Nusselt number is {nusselt}.')
 else:
     print('The flow is laminar.')
     nusselt = 4.364
-    print('The Nusselt number is %f', nusselt)
+    print(f'The Nusselt number is {nusselt}.')
 
 # Internal heat transfer coefficient
 ht_coeff = (water_cond_k * nusselt) / tube_d_i
-print('The internal heat transfer coefficient is %f', ht_coeff)
+print(f'The internal heat transfer coefficient is {ht_coeff}.')
 
 #  overall heat transfer coefficient
 overall_coeff = ((1 / shell_conv_coeff) + tube_d_o * math.log((tube_d_o / 2) / (tube_d_i / 2)) / tube_cond_k
                  + (tube_d_o / tube_d_i) * (1 / ht_coeff)) ** -1
-print('The overall heat transfer coefficient is %f', overall_coeff)
+print(f'The overall heat transfer coefficient is {overall_coeff}.')
 
 #  mass flow rate of water
 m_dot = water_density * water_vel * tube_num * (np.pi * (tube_d_i ** 2) / 2)
-print('The mass flow rate of the water is %f.', m_dot)
+print(f'The mass flow rate of the water is {m_dot}.')
 
 #  external tube area
 tube_area = np.pi * tube_d_o * tube_length * tube_num * tube_passes
-print('The total tube external area is %f.', tube_area)
+print(f'The total tube external area is {tube_area}.')
 
 #  C_min
 c_min = water_cp * m_dot
 
 #  NTU
 ntu = overall_coeff * tube_area / c_min
-print('The NTU value is %f.', ntu)
+print(f'The NTU value is {ntu}.')
 
 #  Effectiveness
 eff = 1 - np.exp(-1 * ntu)
-print('The effectiveness of the exchanger is %f.', eff)
+print(f'The effectiveness of the exchanger is {eff}.')
 
 #  water outlet temp
 out_temp = water_temp + eff * (shell_steam_temp - water_temp)
-print('The outlet water temp is %f.', out_temp)
+print(f'The outlet water temp is {out_temp}.')
 
 #  total heat transfer rate
 ht_rate = c_min * (out_temp - water_temp) / 1000
-print('The total heat transfer rate is %f kW.', ht_rate)
+print(f'The total heat transfer rate is {ht_rate} kW.')
 
 #  Steam condensation rate
 steam_condense = ht_rate / shell_enthalpy
-print('The steam condensation rate is %f kg/s', steam_condense)
+print(f'The steam condensation rate is {steam_condense} kg/s.')
